24d97193a0fa0bf345d666a865062652de4980e6
Ana Beatriz Beckman <ana.beatriz.beckman.falcao@ccc.ufcg.edu.br>
2024-07-29 23:51:11 -0600

    inicio

---
+++ ativiadde01_AnaBeatrizBeckman_NataLira.py
@@ -0,0 +1,13 @@
+class Ambiente:
+    #define o grid 2d, os obstaculos e a posição do objetivo
+    # largura, altura, objetivo, obstaculo
+    pass
+
+class Agente:
+    #possa se mover no grid e tomar decisoes com base na percepção do ambiente
+    # ter uma logica , politica do movimento( mover-se sempre na direção do objetivo)
+    pass
+
+# criar uma função para executar a simulação e permitir que o agente interaja 
+# com o ambiente até encontrar o objetivo ou até um número máximo de passos 
+# ser alcançado.
